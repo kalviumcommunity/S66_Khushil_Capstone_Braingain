6e2f641bd92e3a473d62d879b076043765be5936
khushil <choprakhushil13@gmail.com>
2026-03-30 15:17:26 +0530

    Saving pending changes before pull

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div>
       <h1>BrainGain Project</h1>
-      <p>Frontend Initialized Successfully 🚀</p>
+      <p>Frontend Deployed Successfully 🚀</p>
     </div>
   );
 }
